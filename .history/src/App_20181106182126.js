f36e55d2f1aae858811d6d75e3216d8ddd5f0a53
Jin <water.jj@gmail.com>
2018-11-06 18:26:37 +0800

    init

---
+++ .history/src/App_20181106182126.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path='/' exact component={Home} />>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ .history/src/App_20181106182247.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route path='/' exact component={Home} />>
+      </Router>
+    );
+  }
+}
+
+export default App;
